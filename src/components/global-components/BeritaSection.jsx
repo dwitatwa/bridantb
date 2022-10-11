4c5f5b96f3dd663007e64e201780515e0927e49e
Agung Dwitatwa <agungdwitatwa@gmail.com>
2022-10-11 08:19:30 +0000

    Update Information section dan Berita section, sama tambahan menu khusus untuk PPID

--- src/components/global-components/BeritaSection.jsx
+++ src/components/global-components/BeritaSection.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
-import contentful from "contentful";
-const { createClient } = contentful;
+import { createClient } from "contentful";
+
+// import contentful from "contentful";
+// const { createClient } = contentful;
 
 export default function BeritaSection() {
   const [minimum, setMinimum] = useState(0);
   const [maximum, setMaximum] = useState(4);
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const nextBerita = () => {
     if (maximum <= items.length - 1) {
@@ -32,7 +35,7 @@ export default function BeritaSection() {
       })
       .then((res) => {
         setItems(res.items);
-        console.log(res.items);
+        setIsLoading(false);
       });
   }, []);
 
@@ -44,64 +47,82 @@ export default function BeritaSection() {
             Berita Terbaru
           </h2>
 
-          <a
-            href="#"
-            className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
-          >
-            Selengkapnya
-          </a>
+          <div className="flex gap-4  justify-center">
+            <button
+              href="#"
+              className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-1 md:py-1"
+              onClick={prevBerita}
+            >
+              &larr;
+            </button>
+            <button
+              href="#"
+              className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
+              onClick={nextBerita}
+            >
+              &rarr;
+            </button>
+          </div>
         </div>
       </div>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
-        {items.slice(minimum, maximum).map((item) => (
-          <a
-            href="#"
-            className="group h-48 md:h-64 xl:h-96 flex flex-col bg-blue-100 rounded-lg shadow-lg overflow-hidden relative"
-            key={item.sys.id}
-          >
-            <div className="w-full h-full bg-slate-500 mix-blend-multiply z-40" />
-            <img
-              src={`https:${item.fields.thumbnail.fields.file.url}`}
-              loading="lazy"
-              alt="Photo by Agung Dwitatwa"
-              className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
-            />
+      {isLoading ? (
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
+          <div className="bg-slate-200 h-48 md:h-64 xl:h-96 flex flex-col rounded-lg shadow-lg overflow-hidden relative flex justify-center items-center text-slate-500">
+            Loading...
+          </div>
+          <div className="bg-slate-200 h-48 md:h-64 xl:h-96 flex flex-col rounded-lg shadow-lg overflow-hidden relative flex justify-center items-center text-slate-500">
+            Loading...
+          </div>
+          <div className="bg-slate-200 h-48 md:h-64 xl:h-96 flex flex-col rounded-lg shadow-lg overflow-hidden relative flex justify-center items-center text-slate-500">
+            Loading...
+          </div>
+          <div className="bg-slate-200 h-48 md:h-64 xl:h-96 flex flex-col rounded-lg shadow-lg overflow-hidden relative flex justify-center items-center text-slate-500">
+            Loading...
+          </div>
+        </div>
+      ) : (
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
+          {items.slice(minimum, maximum).map((item) => (
+            <a
+              href="#"
+              className="group h-48 md:h-64 xl:h-96 flex flex-col bg-blue-100 rounded-lg shadow-lg overflow-hidden relative"
+              key={item.sys.id}
+            >
+              <div className="w-full h-full bg-slate-500 mix-blend-multiply z-40" />
+              <img
+                src={`https:${item.fields.thumbnail.fields.file.url}`}
+                loading="lazy"
+                alt="Photo by Agung Dwitatwa"
+                className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+              />
 
-            <div className="absolute p-4 mt-auto z-50 bottom-0">
-              <span className="block text-blue-200 text-sm">
-                {item.fields.tanggalBerita.split("-").reverse().join("-")}
-              </span>
-              <h2 className="text-white text-xl font-semibold transition duration-100 mb-2">
-                {item.fields.judulBerita.substring(0, 50)}{" "}
-                {item.fields.judulBerita.length > 50 && "..."}
-              </h2>
-              <span className="text-blue-300 font-semibold">Read more</span>
-            </div>
-          </a>
-        ))}
-      </div>
-      <div className="flex justify-between items-start sm:items-center gap-8 mt-10">
+              <div className="absolute p-4 mt-auto z-50 bottom-0">
+                <span className="block text-blue-200 text-sm">
+                  {item.fields.tanggalBerita.split("-").reverse().join("-")}
+                </span>
+                <h2 className="text-white text-xl font-semibold transition duration-100 mb-2">
+                  {item.fields.judulBerita.substring(0, 50)}{" "}
+                  {item.fields.judulBerita.length > 50 && "..."}
+                </h2>
+                <span className="text-blue-300 font-semibold">Read more</span>
+              </div>
+            </a>
+          ))}
+        </div>
+      )}
+
+      <div className="flex justify-between items-start sm:items-center gap-8 mt-5">
         <p className="max-w-screen-sm text-gray-500 text-sm lg:text-base">
           Kumpulan berita mengenai pelaksanaan program dan kegiatan seputar
           Badan Riset dan Inovasi Daerah Provinsi Nusa Tenggara Barat
         </p>
 
-        <div className="flex gap-9  justify-center">
-          <button
-            href="#"
-            className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-1 md:py-1"
-            onClick={prevBerita}
-          >
-            &larr;
-          </button>
-          <button
-            href="#"
-            className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
-            onClick={nextBerita}
-          >
-            &rarr;
-          </button>
-        </div>
+        <a
+          href="#"
+          className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
+        >
+          Selengkapnya
+        </a>
       </div>
     </section>
   );
---
+++ src/components/global-components/InformasiSection.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+export default function InformasiSection() {
+  const [minimum, setMinimum] = useState(0);
+  const [maximum, setMaximum] = useState(4);
+
+  const nextBerita = () => {
+    console.log("next");
+  };
+
+  const prevBerita = () => {
+    console.log("prev");
+  };
+
+  return (
+    <section className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mb-10">
+        <div className="flex justify-between items-end gap-4">
+          <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold">
+            Informasi Terbaru
+          </h2>
+          <div className="flex gap-4  justify-center">
+            <button
+              href="#"
+              className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-1 md:py-1"
+              onClick={prevBerita}
+            >
+              &larr;
+            </button>
+            <button
+              href="#"
+              className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
+              onClick={nextBerita}
+            >
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="grid sm:grid-cols-2 gap-6">
+        <a
+          href="#"
+          className="group h-80 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4"
+        >
+          <div className="w-full h-full bg-slate-500 mix-blend-multiply z-40 absolute left-0 bottom-0"></div>
+          <img
+            src="https://images.unsplash.com/photo-1635859890085-ec8cb5466806?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGRvY3VtZW50c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+            loading="lazy"
+            alt="Photo by Fakurian Design"
+            className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+          />
+
+          <div className="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
+
+          <div className="flex flex-col absolute z-50 bottom-5">
+            <span className="text-gray-300">Hasil Seleksi Administrasi</span>
+            <span className="text-white text-lg lg:text-xl font-semibold">
+              Beasiswa NTB Tujuan Malaysia
+            </span>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="group h-80 flex items-end bg-gray-100 rounded-lg overflow-hidden shadow-lg relative p-4"
+        >
+          <div className="w-full h-full bg-slate-500 mix-blend-multiply z-40 absolute left-0 bottom-0"></div>
+          <img
+            src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60"
+            loading="lazy"
+            alt="Photo by Fakurian Design"
+            className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+          />
+
+          <div className="bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50 absolute inset-0 pointer-events-none"></div>
+
+          <div className="flex flex-col absolute z-50 bottom-5">
+            <span className="text-gray-300">Pengumuman Kelas Pemrograman</span>
+            <span className="text-white text-lg lg:text-xl font-semibold">
+              Brida School Academy
+            </span>
+          </div>
+        </a>
+      </div>
+      <div className="flex justify-between items-start sm:items-center gap-8 mt-5">
+        <p className="max-w-screen-sm text-gray-500 text-sm lg:text-base">
+          Kumpulan berita mengenai pelaksanaan program dan kegiatan seputar
+          Badan Riset dan Inovasi Daerah Provinsi Nusa Tenggara Barat
+        </p>
+
+        <a
+          href="#"
+          className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
+        >
+          Selengkapnya
+        </a>
+      </div>
+    </section>
+  );
+}
