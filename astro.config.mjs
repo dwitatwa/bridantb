c27d068ceba0be817e89dabc9fe2ad2585aa2b3d
Agung Dwitatwa <agungdwitatwa@gmail.com>
2022-10-13 03:23:50 +0000

    tambah page profil dan memperbaiki title section setiap page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import path from "path";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@components": path.resolve("./src/components"),
+        "@assets": path.resolve("./src/assets"),
+        "@layouts": path.resolve("./src/layouts"),
+      },
+    },
+  },
 });
