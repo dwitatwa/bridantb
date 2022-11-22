cbbbbdce8256d5c006a97de3f57c42441c59552f
Agung Dwitatwa Achmadi <agungdwitatwa@gmail.com>
2022-11-22 12:14:49 +0800

    static cuy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import path from "path";
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   vite: {
@@ -18,8 +15,4 @@ export default defineConfig({
       },
     },
   },
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
